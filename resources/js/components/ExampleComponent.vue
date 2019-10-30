8f2a8bc82aca9ffa92cd3c54967e96381ef6bbfb
NhanPM <nhanpm@evolableasia.vn>
2019-10-30 11:42:57 +0700

    init laravel6 with vuetify

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-app dark app>
+        <v-app-bar color="deep-purple" dark app>
+            <v-toolbar-title>Vuetify Tutoria</v-toolbar-title>
+        </v-app-bar>
+        <v-content>
+            <router-view/>
+        </v-content>
+    </v-app>
+    <!-- <div id="app">
+                <v-app dark>
+                    <div>
+                        <v-btn color="success" v-on:click="changeProduct">Success</v-btn>
+                        <v-btn color="error">Error</v-btn>
+                        <v-btn color="warning">Warning</v-btn>
+                        <v-btn color="info">Info</v-btn>
+                    </div>
+                    <h1>{{product}}</h1>
+                    <ul>
+                        <li v-for="detail in details">{{detail}}</li>
+                    </ul>
+                    <v-toolbar class="deep-purple"></v-toolbar>
+                    <v-content>
+                    </v-content>
+                </v-app>
+            </div> -->
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        },
+        data() {
+            return {
+                product: 'askss',
+                details: ['a', '1', '2']
+            }
+        },
+        methods: {
+            changeProduct() {
+                this.product = "aa";
+            }
+        }
+    }
+</script>
---
+++ resources/views/companies.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+        <link href="https://unpkg.com/vuetify/dist/vuetify.min.css" rel="stylesheet">      
+        <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">  
+    </head>
+    <body>
+        <div id="app">
+            <example-component></example-component>
+            <tutorial-component></tutorial-component>
+        </div>   
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
